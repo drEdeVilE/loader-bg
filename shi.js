f19c136c74793355fb7f4dd370148daa0c926bea
drEdeVilE <okieimenaaron2005@gmail.com>
2022-07-19 13:15:08 +0100

    load

---
+++ shi.js
@@ -0,0 +1,27 @@
+var progress = 0;
+    document.onreadystatechange = function()
+    {
+        if(document.readyState == "interactive")
+        {
+            var allElement = $("*");
+            var length = allElement.length;
+            for(var i=0;i<length;i++)
+            {
+                set_element(allElement[i],length);
+            }
+        }
+    }
+    function set_element(element,totalElement)
+    {
+        var percentage = 100 / totalElement;
+        if($(element).length==1)
+        {
+            $("#fill").animate({width:progress+percentage+"%"},1000,function(){
+                if(document.getElementById("fill").style.width=="100%")
+                {
+                    $(".main").fadeOut(500);
+                }
+            });
+            progress = progress + percentage;
+        }
+    }
